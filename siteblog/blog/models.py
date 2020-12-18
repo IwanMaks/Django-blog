613f20bb98cca55b59b7532bbcbe16d6730843d7
prostochlen <iwan.maksackov@yandex.ru>
2020-12-18 23:26:48 +0300

    add some posts, categories, tags

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+'''
+Category
+=========
+title, slug
+
+Tag
+=========
+title, slug
+
+Post
+=========
+title, slug, author, content, created_at, photo, views, category, tags
+'''
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название тега')
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок поста')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Адрес фото')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Теги')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
